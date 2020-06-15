481f1ca84d822b53d832938c5193f8eebbe4ffe1
vijay shiyani <vshiyani@meeco.me>
2020-06-15 15:29:10 +1000

    only retrive legacykey if needed.

--- src/decryption/decryption.ts
+++ src/decryption/decryption.ts
@@ -21,7 +21,6 @@ export async function decryptWithKey({
   const { encryptionStrategy } = deSerialized;
   let { decodedPairs } = deSerialized;
   let output: string = '';
-  const keys: string[] = [];
 
   /**
    * Determine if we need to use a derived key or not based on whether or not
@@ -29,23 +28,30 @@ export async function decryptWithKey({
    */
   if (DerivedKeyOptions.usesDerivedKey(serialized)) {
     // Key will now be one derived with Pbkdf
-    keys.push(await _deriveKeyWithOptions(key, serialized));
+    key = await _deriveKeyWithOptions(key, serialized);
 
-    // legacy derived key for backward compatiblity.
-    if (key !== encodeUtf8(key)) {
-      keys.push(await _deriveKeyWithOptions(key, serialized, EncodingVersions.legacy));
-    }
     // Can chop off the last two parts now as they were key data
     decodedPairs = decodedPairs.slice(0, decodedPairs.length - 2);
-  } else {
-    keys.push(key);
   }
 
+  let legacyKey;
   for (let i = 0; i < decodedPairs.length; i += 2) {
     const data: string = decodedPairs[i];
     const artifacts: any = decodedPairs[i + 1];
     const strategy = strategyToAlgorithm(encryptionStrategy);
-    output += _decryptWithKey(keys, data, strategy, artifacts);
+    try {
+      output += _decryptWithKey(legacyKey || key, data, strategy, artifacts);
+    } catch (err) {
+      if (!legacyKey && encodeUtf8(key) !== key) {
+        // Decryption failed with utf-8 key style - retry with legacy utf-16 key format
+        legacyKey = await _deriveKeyWithOptions(key, serialized, EncodingVersions.legacy);
+        i -= 2;
+        continue;
+      } else {
+        // Both utf-8 and utf-16 key formats have failed - bail
+        throw err;
+      }
+    }
   }
   return output;
 }
@@ -65,28 +71,26 @@ function _deriveKeyWithOptions(
 }
 
 function _decryptWithKey(
-  keys: string[],
+  key: string,
   encryptedData: any,
   strategy: CipherStrategy,
   { iv, at, ad }: IEncryptionOptions
 ) {
-  for (const key of keys) {
-    const decipher = cipher.createDecipher(strategy, key);
-    const tagLength = 128;
-    const tag = util.createBuffer(at); // authentication tag from encryption
-    const encrypted = util.createBuffer(encryptedData);
-    decipher.start({
-      iv: util.createBuffer(iv),
-      additionalData: ad,
-      tagLength,
-      tag,
-    });
-    decipher.update(encrypted);
-    const pass = decipher.finish();
-    // pass is false if there was a failure (eg: authentication tag didn't match)
-    if (pass) {
-      return decipher.output.data;
-    }
+  const decipher = cipher.createDecipher(strategy, key);
+  const tagLength = 128;
+  const tag = util.createBuffer(at); // authentication tag from encryption
+  const encrypted = util.createBuffer(encryptedData);
+  decipher.start({
+    iv: util.createBuffer(iv),
+    additionalData: ad,
+    tagLength,
+    tag,
+  });
+  decipher.update(encrypted);
+  const pass = decipher.finish();
+  // pass is false if there was a failure (eg: authentication tag didn't match)
+  if (pass) {
+    return decipher.output.data;
   }
 
   throw new Error('Decryption failed');
