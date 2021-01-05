d36f16bc41a1d4e9cbdc249740b89fd8031f2fa5
vijay shiyani <vshiyani@meeco.me>
2021-01-05 13:23:40 +1100

    updated util method to avoid passing large arg to fromCharCode function

--- test/signing/rsa-signature.spec.ts
+++ test/signing/rsa-signature.spec.ts
@@ -1,11 +1,19 @@
+import { readFileSync } from 'fs';
 import { util } from 'node-forge';
+import { join } from 'path';
 import { generateRSAKeyPair } from '../../src/key-pairs/rsa';
 import {
   loadRsaSignature,
   signWithPrivateKey,
   verifyWithPublicKey,
 } from '../../src/signing/rsa-signature';
-import { encodeSafe64, utf8ToBytes } from '../../src/util';
+import {
+  binaryStringToBytes,
+  bytesToBinaryString,
+  bytesToUtf8,
+  encodeSafe64,
+  utf8ToBytes,
+} from '../../src/util';
 
 describe('signing', () => {
   const data =
@@ -37,6 +45,55 @@ describe('signing', () => {
       const serializedPayload = signatureObj.serialized;
       const loadedSignature = loadRsaSignature(serializedPayload);
       expect(verifyWithPublicKey(keyPair.publicKey, loadedSignature)).toEqual(true);
+      done();
+    } catch (err) {
+      done(err);
+    }
+  });
+
+  it('can sign a large text file', async (done) => {
+    // RSA key generation can take a while...
+    const timeout = 40000;
+    try {
+      jest.setTimeout(timeout);
+    } catch (ex) {}
+    try {
+      jasmine.DEFAULT_TIMEOUT_INTERVAL = timeout;
+    } catch (ex) {}
+    try {
+      const expected = readFileSync(
+        join(__dirname, 'utf8_printable_codepoint_sequence_0-0x1ffff.txt'),
+        'binary'
+      );
+      const bytes = binaryStringToBytes(expected);
+      const keyPair = await generateRSAKeyPair(2048);
+      const signatureObj = signWithPrivateKey(keyPair.privateKey, bytes);
+      const serializedPayload = signatureObj.serialized;
+      expect(serializedPayload.split('.')[3]).toEqual(encodeSafe64(bytesToBinaryString(bytes)));
+      expect(serializedPayload).toMatch(/Rsa2048\./);
+      done();
+    } catch (err) {
+      done(err);
+    }
+  });
+
+  it('can sign a large png file', async (done) => {
+    // RSA key generation can take a while...
+    const timeout = 40000;
+    try {
+      jest.setTimeout(timeout);
+    } catch (ex) {}
+    try {
+      jasmine.DEFAULT_TIMEOUT_INTERVAL = timeout;
+    } catch (ex) {}
+    try {
+      const expected = readFileSync(join(__dirname, '865194.jpg'), 'binary');
+      const keyPair = await generateRSAKeyPair(2048);
+      const bytes = binaryStringToBytes(expected);
+      const signatureObj = signWithPrivateKey(keyPair.privateKey, bytes);
+      const serializedPayload = signatureObj.serialized;
+      expect(serializedPayload.split('.')[3]).toEqual(encodeSafe64(bytesToBinaryString(bytes)));
+      expect(serializedPayload).toMatch(/Rsa2048\./);
       done();
     } catch (err) {
       done(err);
